742981f82a9130f4c417738964190cdf4ce6e1a8
Sarath2013 <sarathkumarpitb2013@gmail.com>
2019-11-13 23:05:51 +0530

    Updates

---
+++ precache-manifest.d9d8c528bd7b9600e99a334815f77947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a1fe8170ba015adcf1ffc5bde74a3a7",
+    "url": "/WeatherForecast/index.html"
+  },
+  {
+    "revision": "ef5df6e376d107fa2400",
+    "url": "/WeatherForecast/static/css/main.1b3a318e.chunk.css"
+  },
+  {
+    "revision": "cab29f1cafbbeea80599",
+    "url": "/WeatherForecast/static/js/2.0c5fef4a.chunk.js"
+  },
+  {
+    "revision": "ef5df6e376d107fa2400",
+    "url": "/WeatherForecast/static/js/main.1cfb9fa3.chunk.js"
+  },
+  {
+    "revision": "c140f49620b5ef422dbb",
+    "url": "/WeatherForecast/static/js/runtime-main.ecbef520.js"
+  }
+]);
